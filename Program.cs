4452b573c59f6307f7eea679b43a5043371239ce
asma20 <jbeli.asma20@gmail.com>
2021-02-16 12:25:39 +0100

    les teste avec if et else

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Partie2_Exercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            Console.WriteLine("votre age svp : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age>18) {
+                Console.WriteLine(" Vous avez {0} ans, vous êtes donc majeur.e. ",age);
+            }
+            else
+            {
+                Console.WriteLine(" Vous avez {0} ans, vous êtes donc mineur.e.", age);
+            }
+           
+        }
+    }
+}
